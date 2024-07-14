e16c0b91007e9aa0db145c0121656ad0df22199b
yh0x1n <yhoxrossell508@gmail.com>
2024-07-14 13:20:04 -0400

    Starting Pandas projects

--- Pandas_projects/pandas_1.py
+++ Pandas_projects/pandas_1.py
@@ -1,1 +1,15 @@
+#Pandas 1: Series
+
 import pandas as pd
+
+materias = pd.Series({'Math' : 15, 'Física' : 18, 'Química'  : 20})
+print(materias) #Diccionario
+
+
+colores = pd.Series(['rojo', 'amarillo', 'azul', 'verde'])
+print(colores) #Lista
+
+numeros = pd.Series([1, 2, 3, 4, 5 ,6 ,7 , 8, 9])
+numeros.size
+print(f"\n {numeros[2 : 7]}") #Lista
+
