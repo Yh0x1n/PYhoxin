fceda02d0dac23d6a0bdf7b7d60ffec50256a6d6
Yh0x1n <yhoxrossell508@gmail.com>
2024-07-21 19:40:28 -0400

    trying to fix library issues

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 #Un archivo de prueba
 import mariadb
+
+try:
+    conn = pymysql.connect(
+
+    host='localhost',
+    port=3306
+    )
+    print('Connection succesful')
+
+except pymysql.Error as e:
+    print(e)
