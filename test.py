e19fa74570943416de451251aff54f0a2631d316
Yh0x1n <yhoxrossell508@gmail.com>
2024-07-21 21:16:28 -0400

    c

--- test.py
+++ test.py
@@ -10,6 +10,12 @@ try:
     database='prueba'
     )
     print('Connection succesful')
+    c = conn.cursor()
+    c.execute('drop database if exists prueba;')
+    c.execute('create database if not exists prueba;')
+    c.execute('use prueba;')
 
 except mariadb.Error as e:
     print(e)
+
+c.close()
