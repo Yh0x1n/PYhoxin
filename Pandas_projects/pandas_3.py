c590481e57e4780b8a1051304a4ac5d3fa3df947
Yh0x1n <yhoxrossell508@gmail.com>
2024-07-27 07:36:10 -0400

    Getting on with Pandas

---
+++ Pandas_projects/pandas_3.py
@@ -0,0 +1,6 @@
+#Pandas 3: Excel a CSV
+
+import pandas as pd
+
+df = pd.read_csv('datos.csv')
+df
--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 #Un archivo de prueba
 import mariadb
+import os
+
+try:
+    os.system('sudo systemctl start mariadb')
+
+except:
+    print('[!] Invalid command.')
 
 try:
     conn = mariadb.connect(
-    user='Yh0x',
+    user='Yh0xr',
     password='12345',
     host='localhost',
     port=3306,
@@ -18,4 +25,11 @@ try:
 except mariadb.Error as e:
     print(e)
 
-c.close()
+c.execute('create table datos('
+          'id int primary key, nombre varchar(20), cedula varchar (20)'
+        ');')
+
+c.execute('insert into datos(id, nombre, cedula)'
+          'values (1, "Yhoxin", "28289663"), (2, "Josmer", "29748403");')
+
+c.execute('select * from datos;')
