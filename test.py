a7b02fc11b3a466baa2b06993bc97c9651342eb2
Yh0x1n <yhoxrossell508@gmail.com>
2024-07-27 07:53:33 -0400

    sexo

--- test.py
+++ test.py
@@ -11,18 +11,11 @@ except:
 
 try:
     conn = mariadb.connect(
-<<<<<<< HEAD
     user='Yh0xr',
     password='12345',
     host='localhost',
     port=3306,
     database='prueba'
-=======
-        user = 'Yh0xr',
-        password = '12345',
-        host = 'localhost',
-        port = 3306,
->>>>>>> 4dfc481a18bf5ff53390cd3ff93ef0eabcbf9c26
     )
     c = conn.cursor()
     c.execute('DROP DATABASE IF EXISTS prueba;')
@@ -31,7 +24,6 @@ try:
 
     print('Connection succesful')
 except mariadb.Error as e:
-<<<<<<< HEAD
     print(e)
 
 c.execute('create table datos('
@@ -42,6 +34,3 @@ c.execute('insert into datos(id, nombre, cedula)'
           'values (1, "Yhoxin", "28289663"), (2, "Josmer", "29748403");')
 
 c.execute('select * from datos;')
-=======
-    print(e)
->>>>>>> 4dfc481a18bf5ff53390cd3ff93ef0eabcbf9c26
